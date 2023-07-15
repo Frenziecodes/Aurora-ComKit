a54d17262ca4e6666a939375ea62e7b9fb05579e
Frenziecodes <coderflame3@gmail.com>
2023-07-15 10:55:56 +0300

    adding FeatureThree

--- src/AuroraComp/Features.jsx
+++ src/AuroraComp/Features.jsx
@@ -1,11 +1,13 @@
 import FeatureOne from "./Features/FeatureOne"
 import FeatureTwo from "./Features/Featuretwo"
+import FeatureThree from "./Features/FeatureThree"
 function AuroraFeatures(){
     return(
         <section className="min-h-screen pt-20 px-6 flex justify-center">
             <div className="mb-12 text-gray-800 text-center max-w-[1200px]">
                 <FeatureOne/>                
                 <FeatureTwo/>
+                <FeatureThree/>
             </div>
             
         </section>
